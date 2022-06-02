9d7f49cb6b66a5b5a43d04602b5be6d4a7c3f18c
spiism <sp.iism@gmail.com>
2022-06-02 13:39:56 +1200

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./styles.css";
+import { useEffect } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  useEffect(() => {
+    register("emailContent", { required: true, minLength: 11 });
+  }, [register]);
+
+  const onEditorStateChange = (editorState) => {
+    setValue("emailContent", editorState);
+  };
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const test = ` <ol><li><strong><em><u>hom nay la 1 ngay binh thuong<span class="ql-cursor">﻿</span></u></em></strong></li></ol> `;
+
+  const editorContent = watch("emailContent");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactQuill
+        theme="snow"
+        value={editorContent}
+        onChange={onEditorStateChange}
+      />{" "}
+      <p className="Error">{errors.emailContent && "Enter valid content"}</p>
+      <input type="submit" onClick={handleSubmit(onSubmit)} />
+      <p>{editorContent}</p>
     </div>
   );
 }
