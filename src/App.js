2a5561cf9f7d125d97d0dae16380ef1ee30258fd
spiism <sp.iism@gmail.com>
2022-06-02 14:22:57 +1200

    update

--- src/App.js
+++ src/App.js
@@ -27,17 +27,65 @@ function App() {
     console.log(data);
   };
 
+  const modules = {
+    toolbar: {
+      container: [
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [{ size: ["small", false, "large", "huge"] }, { color: [] }],
+        [
+          { list: "ordered" },
+          { list: "bullet" },
+          { indent: "-1" },
+          { indent: "+1" },
+          { align: [] },
+        ],
+        ["link", "image", "video"],
+        ["clean"],
+      ],
+      // handlers: { image: this.imageHandler },
+    },
+    clipboard: { matchVisual: false },
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "size",
+    "color",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "video",
+    "align",
+  ];
   const test = ` <ol><li><strong><em><u>hom nay la 1 ngay binh thuong<span class="ql-cursor">﻿</span></u></em></strong></li></ol> `;
 
   const editorContent = watch("emailContent");
   return (
     <div className="App">
       <ReactQuill
-        theme="snow"
         value={editorContent}
         onChange={onEditorStateChange}
-      />{" "}
-      <p className="Error">{errors.emailContent && "Enter valid content"}</p>
+        formats={formats}
+        modules={modules}
+      />
+      <br />
+      <br />
+      <br />
+      {/* <ReactQuill
+        readOnly={true}
+        value={editorContent}
+        onChange={onEditorStateChange}
+        formats={formats}
+        modules={modules}
+      />{" "} */}
+      {/* <p className="Error">{errors.emailContent && "Enter valid content"}</p> */}
       <input type="submit" onClick={handleSubmit(onSubmit)} />
       <p>{editorContent}</p>
     </div>
